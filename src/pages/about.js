db3b1e428d40b46a0ff67fbf37f29e3ffb2f9ed6
ojds2022 <odiazstrandberg@gmail.com>
2023-10-30 10:57:39 -0700

    feat: display daily nutritional totals, updated about us page

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,9 +12,9 @@ const About = () => {
       <div className="flex flex-col items-center text-center aboutBottom h-3/5 bg-pink text-navy-blue">
         <h1 className="py-2 text-3xl xs:text-4xl md:text-4xl md:py-5 lg:text-5xl lg:py-8 xl:text-6xl">ABOUT US</h1>
         <p className="w-4/5 xxs:text-lg xs:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-          HappyPlate is an app that provides in-depth nutritional information on
-          thousands of foods and beverages! Search your favorite meals, snacks, and 
-          drinks and instantly get the nutritional information you need!
+          HappyPlate is an app that provides nutritional recommenations to users based on
+          several different factors and calculates your daily caloric needs. Search thousands 
+          of foods and beverages and get the in-depth nutritional information you need!
         </p>
       </div>
     </div>
--- src/pages/nutritionBreakdown.js
+++ src/pages/nutritionBreakdown.js
@@ -9,7 +9,7 @@ import { useTdee } from "../components/TdeeContext";
 
 ChartJS.register(ArcElement, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-export const options = {
+export const barChartOptions = {
   responsive: true,
   plugins: {
     legend: {
@@ -18,20 +18,6 @@ export const options = {
     title: {
       display: false,
       text: 'Nutritional Breakdown',
-    },
-  },
-};
-
-export const barOptions = {
-  
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'bottom',
-    },
-    title: {
-      display: false,
-      text: 'Chart.js Bar Chart',
     },
   },
   interaction: {
@@ -46,42 +32,49 @@ export const barOptions = {
       stacked: true,
     },
   },
+  
+};
+
+export const pieChartOptions = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'bottom',
+    },
+    title: {
+      display: false,
+      text: 'Nutritional Percentage',
+    },
+  },
 };
 
 const NutritionBreakdown = () => {
-  const tdee = 3000;
-  //const { tdee } = useTdee();
+  const { tdee } = useTdee();
   const [showLabel, setShowLabel] = useState(false);
   const [name, setName] = useState("");
 
-  {/*const [servingSize, setServingSize] = useState("");
-  const [calories, setCalories] = useState("");
-  const [fatTotal, setFatTotal] = useState("");
-  const [fatSat, setFatSat] = useState("");
-  const [cholesterol, setCholesterol] = useState("");
-  const [sodium, setSodium] = useState("");
-  const [carbs, setCarbs] = useState("");
-  const [fiber, setFiber] = useState("");
-  const [sugar, setSugar] = useState("");
-  const [protein, setProtein] = useState("");
-const [potassium, setPotassium] = useState("");*/}
-
-  const [chartData, setChartData] = useState({
-    labels: [],
-    datasets: [
-      {
-        label: '',
-        data: [],
-        backgroundColor: [],
-      },
-    ],
-  });
+  const [previousCals, setPreviousCals] = useState('');
+  const [previousCarbs, setPreviousCarbs] = useState('');
+  const [previousProtein, setPreviousProtein] = useState('');
+  const [previousFats, setPreviousFats] = useState('');
+  const [previousSugar, setPreviousSugar] = useState('');
 
   const [barChartData, setBarChartData] = useState({
     labels: [],
     datasets: [
       {
         label: 'Dataset 1',
+        data: [],
+        backgroundColor: [],
+      },
+    ],
+  });
+
+  const [pieChartData, setPieChartData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: '',
         data: [],
         backgroundColor: [],
       },
@@ -109,40 +102,35 @@ const [potassium, setPotassium] = useState("");*/}
   };
 
   const postNutrition = (data) => {
-    console.log(tdee);
-    
     setName(data.name);
 
-    setChartData({
-      labels: ['Protein', 'Carbs', 'Fats', 'Sugars'],
-      datasets: [
-        {
-          label: 'percentage',
-          data: [data.protein_g, data.carbohydrates_total_g, data.fat_total_g, data.sugar_g],
-          backgroundColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-          ],
-          hoverBackgroundColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-          ],
-        },
-      ],
-    });
+    //takes the input from user and gives the data on current food/beverage
+    const totalCals = data.calories;
+    const carbCals = data.carbohydrates_total_g * 4;
+    const proteinCals = data.protein_g * 4;
+    const fatCals = data.fat_total_g * 9;
+    const sugarCals = data.sugar_g * 4;
+
+    //takes the data from the current food/beverage and adds it to the previous food/beverage
+    setPreviousCals(Number(totalCals) + Number(previousCals));
+    setPreviousCarbs(Number(carbCals) + Number(previousCarbs));
+    setPreviousProtein(Number(proteinCals) + Number(previousProtein));
+    setPreviousFats(Number(fatCals) + Number(previousFats));
+    setPreviousSugar(Number(sugarCals) + Number(previousSugar));
+
+    //adds up the total of the previous food/beverage data and adds it to the grand total data
+    const cumulativeCals = previousCals + totalCals;
+    const cumulativeCarbs = previousCarbs + carbCals;
+    const cumulativeProteins = previousProtein + proteinCals;
+    const cumulativeFats = previousFats + fatCals;
+    const cumulativeSugars = previousSugar + sugarCals;
 
     setBarChartData({
-      labels: ['Calories', 'Carbs', 'Protein', 'Fats', 'Sugars',],
+      labels: ['Calories', 'Carbs', 'Protein', 'Fats', 'Sugars',], 
       datasets: [
         {
-          label: `${data.name}`,
-          data: [data.calories, data.carbohydrates_total_g, data.protein_g, data.fat_total_g, data.sugar_g,],
+          label: 'Current total',
+          data: [cumulativeCals, cumulativeCarbs, cumulativeProteins, cumulativeFats, cumulativeSugars],
           backgroundColor: [
             'rgba(0, 255, 0, 1)',
             'rgba(0, 255, 0, 1)',
@@ -161,13 +149,38 @@ const [potassium, setPotassium] = useState("");*/}
         },
         {
           label: 'RDA',
-          data: [tdee, tdee * 0.55, tdee * 0.15, tdee * 0.3, tdee * .1,],
+          data: [tdee - cumulativeCals, (tdee * 0.55) - cumulativeCarbs, (tdee * 0.15) - cumulativeProteins, (tdee * 0.3) - cumulativeFats, (tdee * .1) - cumulativeSugars],
           backgroundColor: [
+            'rgba(153, 102, 255, 1)',
             'rgba(255, 99, 132, 1)',
             'rgba(54, 162, 235, 1)',
             'rgba(255, 206, 86, 1)',
             'rgba(75, 192, 192, 1)',
+          ],
+          hoverBackgroundColor: [
             'rgba(153, 102, 255, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+          ],
+          stack: 'Stack 0',
+        },
+      ],
+    });
+
+    setPieChartData({
+      labels: ['Carbs','Protein', 'Fats', 'Sugars'],
+      datasets: [
+        {
+          label: 'percentage',
+          data: [data.carbohydrates_total_g, data.protein_g, data.fat_total_g, data.sugar_g],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.8)',
+            'rgba(54, 162, 235, 0.8)',
+            'rgba(255, 206, 86, 0.8)',
+            'rgba(75, 192, 192, 0.8)',
+            'rgba(153, 102, 255, 0.8)',
           ],
           hoverBackgroundColor: [
             'rgba(255, 99, 132, 1)',
@@ -176,12 +189,11 @@ const [potassium, setPotassium] = useState("");*/}
             'rgba(75, 192, 192, 1)',
             'rgba(153, 102, 255, 1)',
           ],
-          stack: 'Stack 0',
         },
       ],
     });
 
-  setShowLabel(!showLabel);
+  setShowLabel(true);
   };
   
   
@@ -199,34 +211,26 @@ const [potassium, setPotassium] = useState("");*/}
 
   return (
     <div
-    className="h-screen bg-center bg-50% bg-sky-blue home"
+    className="bg-center bg-50% bg-sky-blue home"
     style={{ backgroundImage: `url(${BackgroundImage})` }}
     >
     {showLabel === true ? (
-        <>
-        <div className='flex py-28'>
+        <div className='flex flex-col py-28'>
           <div className="grow">
             <div className="text-center"><h1 className="mb-1 text-3xl font-bold text-white capitalize">{name}</h1></div>
             <div className="m-5 rounded-md drop-shadow-xl bg-hot-pink"><h2 className="text-center text-white text-lg font-bold .home">Macronutrient Targets</h2></div>
-            <div className=""><Bar options={barOptions} data={barChartData} /></div>
+            <div className="mx-2 rounded bg-opacity-90 bg-sky-blue"><Bar options={barChartOptions} data={barChartData} /></div>
           </div>
-          <div className="grow">
+          <div className="mt-16 grow">
             <div className="m-5 rounded-md drop-shadow-xl bg-hot-pink"><h2 className="text-center text-white text-xl font-bold .home">Nutritional Percentage</h2></div>
-            <div className=""><Pie options={options} data={chartData} /></div>
-            {/* <div className="my-5 rounded-md drop-shadow-xl bg-hot-pink"><h2 className="text-center text-white text-xl font-bold .home">More Data</h2></div>
-            <div className=""><Bar options={barOptions} data={barChartData} /></div> */}
+            <div className="mx-3 rounded bg-opacity-90 bg-sky-blue"><Pie options={pieChartOptions} data={pieChartData} /></div>
+          </div>
+          <div className="mx-auto mt-5">
+            <button id='searchAgain' className='w-24 border-0 rounded cursor-pointer xxs:text-xl xxs:w-32 xxs:h-8 xl:w-40 xl:h-10 xl:text-xl 3xl:w-72 3xl:h-20 3xl:text-4xl xl:rounded-lg 3xl:rounded-2xl' onClick={() => setShowLabel(false)}>Add more</button>
           </div>
         </div>
-        {/* <div className="z-20 grid content-center">
-            <span className='place-self-center'>
-                <Link to='/'>
-                    <button id='searchAgain' className='w-24 h-6 mt-4 ml-1 border-0 rounded cursor-pointer xxs:text-xl xxs:w-32 xxs:h-8 xl:w-40 xl:h-10 xl:text-xl 3xl:w-72 3xl:h-20 3xl:text-4xl xl:rounded-lg 3xl:rounded-2xl' >Add more</button>
-                </Link>
-            </span>
-        </div> */}
-        </>
     ) : (
-        <div className="grid h-full drop-shadow-xl">
+        <div className="grid h-screen drop-shadow-xl">
           <div className="w-10/12 py-5 mb-32 text-center place-self-center bg-corn-silk md:w-8/12 rounded-xl">
             <h2 className="mx-auto mb-3 text-xl rounded xl:text-3xl 3xl:text-6xl 3xl:p-4 text-hot-pink">
                 Enter a food or beverage to get a nutritional percentage breakdown!
