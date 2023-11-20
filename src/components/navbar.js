9e12803873be24c4b381e82c241281089ae2e9bf
ojds2022 <odiazstrandberg@gmail.com>
2023-11-20 15:26:10 -0800

    navbar responsiveness

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,16 +12,16 @@ const Navbar = () => {
   };
 
   return (
-    <div className="fixed top-0 z-50 flex flex-row w-full h-20 xl:h-24 2xl:h-40 navbar bg-corn-silk">
+    <div className="fixed top-0 z-50 flex flex-row w-full h-20 xl:h-28 2xl:h-40 navbar bg-corn-silk">
       {openLinks ? (
-        <div className="flex items-center justify-between px-6 md:justify-around grow">
+        <div className="flex items-center justify-between px-6 grow">
           <Link to="/">
-            <span className="text-lg text-hot-pink hover:text-pale-green md:text-xl lg:text-2xl xl:text-3xl 2xl:text-5xl">
+            <span className="text-lg text-hot-pink hover:text-pale-green xxs:text-2xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl">
               Home
             </span>
           </Link>
           <Link to="/about">
-            <span className="text-lg text-hot-pink hover:text-pale-green md:text-xl lg:text-2xl xl:text-3xl 2xl:text-5xl">
+            <span className="text-lg text-hot-pink hover:text-pale-green xxs:text-2xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl">
               About
             </span>
           </Link>
@@ -35,7 +35,7 @@ const Navbar = () => {
       ) : (
         <div className="flex items-center justify-between px-6 grow">
           <Link to="/">
-            <img id="plateLogo" className="w-14 xl:w-20 3xl:w-32" src={Logo} alt="logo" />
+            <img id="plateLogo" className="w-14 lg:w-20 xl:w-24 2xl:w-28 3xl:w-32" src={Logo} alt="logo" />
           </Link>
           <button
             className="bg-transparent border-0 cursor-pointer burger"
